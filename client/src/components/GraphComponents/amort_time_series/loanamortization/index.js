f6244d8919765e2cf98386720af3f6cc13ff2a44
RobS <51037713+RobS30@users.noreply.github.com>
2019-11-16 14:23:37 -0600

    moved and updated ammortization logic, added pie component, and ammoritization chart

---
+++ client/src/components/GraphComponents/amort_time_series/loanamortization/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var Mortgage = require("./mortgage");
+
+// var amount = 30000;
+// var interest = 0.06;
+// var years = 10;
+var afterMonths = 6;
+
+export function amort(amount, interest, years) {
+  var mortgage = new Mortgage(amount, interest, years);
+  var payment = mortgage.fixedMonthlyPayment();
+  var remaining = mortgage.remainingLoanBalance(afterMonths);
+  var amortTable = mortgage.amortizationTable();
+  var amortList = amortTable.map(function(row, i) {
+    return [i, row[4]];
+  });
+  console.log("Fixed monthly payments: " + payment);
+  console.log(
+    "Remaining balance after " + afterMonths + " months: " + remaining
+  );
+  let temp = [['Month', 'Balance']];
+  amortList.forEach(Element => {
+    temp.push(Element)
+  })
+  console.log("Amortization table:");
+  
+  return temp;
+}
+
+//amort(26000, 0.055, 6);
