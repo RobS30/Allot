99d609653d0210e4407f76bb5f7225d707506e9c
Fawzi Alami <fawzi.alami@gmail.com>
2019-11-17 16:48:52 -0600

    cash flow component

--- client/src/components/GraphComponents/net_income/netincomecomponent.js
+++ client/src/components/GraphComponents/net_income/netincomecomponent.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from 'axios';
 import Chart from 'react-google-charts';
 
-class netincomebar extends React.Component {
+class NetIncomeBarComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -19,71 +19,55 @@ class netincomebar extends React.Component {
     axios.defaults.headers.common["Authorization"] = sessionStorage.getItem(
       "jwtToken"
     );
-    
-    axios.get('/api/expensechart/' + id).then(res => {
-    // this needs to be updated to pull categories along with the number of times they occur       
-      let data = (res.data.balances, res.data.rate);
+
+    axios.get('/api/cashflow/' + user.id).then(res => {
+      // this needs to be updated to pull categories along with the number of times they occur       
+      let temp = [['Month', 'Income', 'Expenses']];
+      for (let i=0; i<res.data.length; i++){
+        temp.push(res.data[i]);
+      }
+      console.log('cashflow', temp)
       this.setState({
-        chartData: data
+        chartData: temp
       });
     })
   }
 
-render() {
+  render() {
     return (
       <div>
-        <button
-          onClick={() => {
-            const { google, chartEditor, chartWrapper } = component.state;
-            if (
-              chartWrapper === null ||
-              google === null ||
-              chartEditor === null
-            )
-              return;
-              chartEditor.openDialog(chartWrapper);
-              google.visualization.events.addListener(chartEditor, "ok", () => {
-                const newChartWrapper = chartEditor.getChartWrapper();
-                newChartWrapper.draw();
-                const newChartOptions = newChartWrapper.getOptions();
-                const newChartType = newChartWrapper.getChartType();
-                console.log("Chart type changed to ", newChartType);
-                console.log("Chart options changed to ", newChartOptions);
-              });
-            }}
-          >
-</button>
-<Chart
-  width={'500px'}
-  height={'300px'}
-  chartType="BarChart"
-  loader={<div>Loading Chart</div>}
-  data={[
-    this.state.chartData
-    //   data needs to be in this for
-    // ['City', '2010 Population', '2000 Population'],
-    // ['New York City, NY', 8175000, 8008000],
-    // ['Los Angeles, CA', 3792000, 3694000],
-    // ['Chicago, IL', 2695000, 2896000],
-    // ['Houston, TX', 2099000, 1953000],
-    // ['Philadelphia, PA', 1526000, 1517000],
-  ]}
-  options={{
-    title: 'Monthly Cash Flow',
-    chartArea: { width: '50%' },
-    hAxis: {
-      title: 'Income',
-      minValue: 0,
-    },
-    vAxis: {
-      title: 'Expenses',
-    },
-  }}
-  // For tests
-  rootProps={{ 'data-testid': '1' }}
-/>
-</div>
+        <Chart
+          width={'500px'}
+          height={'300px'}
+          chartType="BarChart"
+          loader={<div>Loading Chart</div>}
+          data={this.state.chartData
+            // ['City', '2010 Population', '2000 Population'],
+            // ['New York City, NY', 8175000, 8008000],
+            // ['Los Angeles, CA', 3792000, 3694000],
+            // ['Chicago, IL', 2695000, 2896000],
+            // ['Houston, TX', 2099000, 1953000],
+            // ['Philadelphia, PA', 1526000, 1517000],
+          }
+            //   data needs to be in this for
+          // }
+          options={{
+            title: 'Monthly Cash Flow',
+            chartArea: { width: '50%' },
+            hAxis: {
+              title: 'Income',
+              minValue: 0,
+            },
+            vAxis: {
+              title: 'Expenses',
+            },
+          }}
+          // For tests
+          rootProps={{ 'data-testid': '1' }}
+        />
+      </div>
     );
   }
 }
 
+export default NetIncomeBarComponent;
