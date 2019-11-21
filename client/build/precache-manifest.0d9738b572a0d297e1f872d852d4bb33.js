94141beb2bafa59d1ef78eef948348399c49494c
Fawzi Alami <fawzi.alami@gmail.com>
2019-11-20 18:41:14 -0600

    committing to heroku

---
+++ client/build/precache-manifest.0d9738b572a0d297e1f872d852d4bb33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538be3633ae91a41a1fc91befc231ec6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1901f69086cb33da2ab9",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "0f9ea901a2c82d86e710",
+    "url": "/static/css/main.d22a2663.chunk.css"
+  },
+  {
+    "revision": "1901f69086cb33da2ab9",
+    "url": "/static/js/2.4b389be3.chunk.js"
+  },
+  {
+    "revision": "0f9ea901a2c82d86e710",
+    "url": "/static/js/main.1dee4a69.chunk.js"
+  },
+  {
+    "revision": "498a6017d9da24abad79",
+    "url": "/static/js/runtime-main.c58920cd.js"
+  }
+]);
